d9e27817eee982c93d8318b3f70e853078aee484
ruanazevedo <ruann.azevedo@outlook.com>
2022-06-26 17:53:55 -0300

    corrigindo bug de funcoes e comecando a implementar a funcao disco_grava

--- Gerenciador.c
+++ Gerenciador.c
@@ -92,7 +92,7 @@ void imprimeSetoresLivres(Disco *d)
     printf("\n");
 }
 
-bool inserir_setor(Disco *disco, unsigned long inicio, unsigned long fim)
+bool inserir_setor(NoSetor *setorSentinela, unsigned long inicio, unsigned long fim)
 {
     NoSetor *new = (NoSetor *)malloc(sizeof(NoSetor));
 
@@ -102,8 +102,8 @@ bool inserir_setor(Disco *disco, unsigned long inicio, unsigned long fim)
         return false;
 
     // checa conflito de espaços
-    NoSetor *aux = disco->setoresLivres->prox;
-    while (aux != disco->setoresLivres)
+    NoSetor *aux = setorSentinela->prox;
+    while (aux != setorSentinela)
     {
         if (inicio == aux->inicio || fim == aux->fim) // sobreposição de valores
             return false;
@@ -119,10 +119,23 @@ bool inserir_setor(Disco *disco, unsigned long inicio, unsigned long fim)
     new->fim = fim;
 
     // ajusta as posições dos nós setores
-    new->prox = disco->setoresLivres;
-    disco->setoresLivres->ant->prox = new;
-    new->ant = disco->setoresLivres->ant;
-    disco->setoresLivres->ant = new;
+    new->prox = setorSentinela;
+    setorSentinela->ant->prox = new;
+    new->ant = setorSentinela->ant;
+    setorSentinela->ant = new;
+    return true;
+}
+
+bool criar_listaSetores(NoSetor **setorSentinela)
+{
+    NoSetor *new = (NoSetor *)malloc(sizeof(NoSetor));
+
+    if (new == NULL)
+        return false;
+
+    new->prox = new;
+    new->ant = new;
+    *setorSentinela = new;
     return true;
 }
 
@@ -142,22 +155,20 @@ Disco *disco_cria(char *nome, long int tamanho)
     disco->qtdArquivos = 0;
 
     // Cria setores livres
-    NoSetor *newSetor = (NoSetor *)malloc(sizeof(NoSetor));
-    if (newSetor == NULL)
+    if (!criar_listaSetores(&(disco->setoresLivres)))
     {
-        perror("theres is not enough memory to create >>sectors<<");
+        perror("Unable to create a list of sectors of empty space >>List Sectors<<");
         return NULL;
     }
-    newSetor->prox = newSetor;
-    newSetor->ant = newSetor;
-    disco->setoresLivres = newSetor;
-    if (!inserir_setor(disco, 0, tamanho - 1))
+
+    // Insere espaco livre
+    if (!inserir_setor(disco->setoresLivres, 0, tamanho - 1))
     {
         perror("Unable to create a sector of empty space >>empty sector<<");
-        return false;
+        return NULL;
     }
 
-    // Cria arquivo
+    // Cria lista de arquivos
     NoArquivo *newArquivo = (NoArquivo *)malloc(sizeof(NoArquivo));
     if (newArquivo == NULL)
     {
@@ -175,9 +186,43 @@ Disco *disco_cria(char *nome, long int tamanho)
 }
 
 // nome arquivo deve conter o caminho absoluto ou relativo do arquivo
-bool disco_grava(Disco *d, char *arquivo);
-bool disco_remove(Disco *d, char *nome);                         // somente o nome do arquivo sem o caminho
-bool disco_recupera(Disco *d, char *nome, char *arquivoDestino); // nome arquivo deve conter o caminho absoluto ou relativo do arquivo
+bool disco_grava(Disco *d, char *arquivo)
+{
+    // abre arquivo
+    FILE *arq = fopen(arquivo, "r");
+    unsigned long tamArquivo = getTamanhoArquivo(arq);
+
+    // cria nó arquivo e implementa os dados
+    NoArquivo *newArquivo = (NoArquivo *)malloc(sizeof(NoArquivo));
+    strcpy(newArquivo->nome, arquivo);
+    newArquivo->tam = tamArquivo;
+
+    // ajusta os nós arquivos na lista de arquivos
+    newArquivo->prox = d->arquivos;
+    d->arquivos->ant->prox = newArquivo;
+    newArquivo->ant = d->arquivos->ant;
+    d->arquivos->ant = newArquivo;
+
+    // Cria lista de setores
+    if (!criar_listaSetores(newArquivo->setores))
+    {
+        perror("Unable to create a list of sectors >>List sectors<<");
+        return false;
+    }
+
+    // Aqui lá se foi o boi com a corda !!!!
+    // TODO verificar espaços suficientes livres
+    // TODO Cria setor do arquivo
+    // TODO copia o conteudo do arquivo para a memoria
+    // TODO grava no disco
+    fclose(arq);
+}
+
+// somente o nome do arquivo sem o caminho
+bool disco_remove(Disco *d, char *nome);
+
+// nome arquivo deve conter o caminho absoluto ou relativo do arquivo
+bool disco_recupera(Disco *d, char *nome, char *arquivoDestino);
 
 void disco_lista(Disco *d)
 {
--- main.c
+++ main.c
@@ -2,12 +2,19 @@
 
 int main()
 {
+    // verifica a existencia do disco
     Disco *disco = disco_cria("dados de arquivos", 15);
     if (disco != NULL)
         printf("\nexiste\n");
     else
         printf("\nNULL\n");
 
+    // checando os arquivos
+    char path[50] = "arquivos/grupo.txt";
+    unsigned long tamFile = getTamanhoArquivo(fopen(path, "r"));
+    printf("tamanho do arquivo: %d bytes\n", tamFile);
+
+    // verificando os espaços livres dentro do disco
     imprimeSetoresLivres(disco);
     printf("\n");
 
